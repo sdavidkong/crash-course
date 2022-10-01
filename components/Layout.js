064bb6dde54ee1e7753a5b276c02015c2895fb70
S David Kong <SDAVIDKONG@gmail.com>
2022-09-30 22:30:32 -0400

    layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "../styles/Layout.module.css";
+import Nav from "./Nav";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        {children}
-        <h3>wooooop woooop</h3>
-      </main>
-    </div>
+    <>
+      <Nav></Nav>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {children}
+          <h3>wooooop woooop</h3>
+        </main>
+      </div>
+    </>
   );
 };
 
