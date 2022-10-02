8604d4d98c38592828e06673369c21f712ee305d
S David Kong <SDAVIDKONG@gmail.com>
2022-10-02 12:32:17 -0400

    added style and components

---
+++ components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import articleStyles from "../styles/Article.module.css";
+
+const ArticleList = ({ articles }) => {
+  return (
+    <div className="articleStyles.grid">
+      {articles.map((article) => (
+        <h3>{article.title}</h3>
+      ))}
+    </div>
+  );
+};
+
+export default ArticleList;
